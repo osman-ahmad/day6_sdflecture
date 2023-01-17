ef0ee5f1f993f4389cf4439b39478147939cc056
osman-ahmad <satriamillenium@yahoo.com.sg>
2023-01-17 11:30:54 +0800

    Threading using executor service

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  */
@@ -12,6 +15,49 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+
+        // Thread thread1 = new Thread(new Runnable() {
+
+        //     @Override
+        //     public void run(){
+        //         for(int i = 0 ;i < 20; i++){
+        //             System.out.println(Thread.currentThread().getName() + "\tRunnable ..." + i);
+        //         }
+        //     }
+        // });
+        // thread1.start();
+
+        MyRunnableImplementation mRI = new MyRunnableImplementation("task1");
+        MyRunnableImplementation mRI2 = new MyRunnableImplementation("task2");
+        MyRunnableImplementation mRI3 = new MyRunnableImplementation("task3");
+        MyRunnableImplementation mRI4 = new MyRunnableImplementation("task4");
+        MyRunnableImplementation mRI5 = new MyRunnableImplementation("task5");
+        // Thread thread2 = new Thread(mRI);
+        // thread2.start();
+
+        // Thread thread3 = new Thread(mRI);
+        // thread3.start();// 
+
+        // ExecutorService executorService = Executors.newSingleThreadExecutor();
+        // executorService.execute(mRI);
+        // executorService.execute(mRI2);
+        // executorService.shutdown();
+
+        // ExecutorService executorService = Executors.newFixedThreadPool(3);
+        // executorService.execute(mRI);
+        // executorService.execute(mRI2);
+        // executorService.execute(mRI3);
+        // executorService.execute(mRI4);
+        // executorService.execute(mRI5);
+        // executorService.shutdown();
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(mRI);
+        executorService.execute(mRI2);
+        executorService.execute(mRI3);
+        executorService.execute(mRI4);
+        executorService.execute(mRI5);
+        executorService.shutdown();
     }
 }
---
+++ src/main/java/sg/edu/nus/iss/MyRunnableImplementation.java
@@ -0,0 +1,21 @@
+package sg.edu.nus.iss;
+
+public class MyRunnableImplementation implements Runnable {
+
+    private String taskName;
+
+    public MyRunnableImplementation() {
+    }
+
+    public MyRunnableImplementation(String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + "\t" + taskName +
+            "\t Runnable... " + i);
+        }
+    }
+}
